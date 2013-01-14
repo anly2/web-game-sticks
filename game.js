04b2f6f4a7d3608abbee436a475427a68aa03a1b
Anko Anchev <anko.anchev@gmail.com>
2013-01-14 17:26:50 +0000

    Reworked the Grid layout
    + Redid the grid_build-ing
    ++ There are no bounding blocks anymore
    ++ All is sticks perfectly matched with margins and size. No pointless
    elements
    
    + Added an implementation of the notice system ("system" <- lol)
    ++ Changes in turns are now properly announced with the player names
    ++ Upon filling the entire grid a game over is declared and the winner is
    announced.
    +++ .leadingPlayer()  can actually be called at any time.

--- game.js
+++ game.js
@@ -2,6 +2,9 @@ game = new Object(); //This is the global class object
 
 
 game.param = new Object();
+game.param.gridSize = 10; //default value, should be updated by .build_grid()
+game.param.countSticks = 199; //default value for gridSize==10 (2s^2-1),  should be updated by .build_grid()
+
 game.param.default = new Object();
 game.param.default.gridSize = 10;
 
@@ -10,28 +13,31 @@ game.build_grid = function (size, sReturn){
    if (!size)
       size = game.param.default.gridSize;
 
+   game.param.gridSize = size;
+   game.param.countSticks = size*(size*2 - 2);
+
    var grid_code = '';
    var r, c;
 
-   grid_code += '<table class="game grid">'+"\n";
+   grid_code += ''+"\n";
 
-   for (r=0; r<size; r++){
-      grid_code += '   '+'<tr>'+"\n";
+   for (r=0; r<size; r++) {
+      grid_code += "\n";
 
-      for (c=0; c<size; c++){
-         grid_code += '      '+'<td id="'+r+':'+c+'" class="block">'+"\n";
-         grid_code += '         '+'<div class="owner"></div>'+"\n";
-         grid_code += '         '+'<span class="top    ghost stick" name="stick"></span>'+"\n";
-         grid_code += '         '+'<span class="right  ghost stick" name="stick"></span>'+"\n";
-         grid_code += '         '+'<span class="bottom ghost stick" name="stick"></span>'+"\n";
-         grid_code += '         '+'<span class="left   ghost stick" name="stick"></span>'+"\n";
-         grid_code += '      '+'</td>'+"\n";
+      for (c=0; c<size-1; c++) {
+         grid_code += '<div id="'+r+':'+c+'" class="horizontal stick" name="stick"></div>'+"\n";
       }
+      grid_code += ' <br /><!-- Complete horizontal row -->'+"\n";
 
-      grid_code += '   '+'</tr>'+"\n";
+      if (r < size-1) {
+         for (c=0; c < size-1; c++) {
+            grid_code += '<div id="'+r+'.5:'+c+'" class="vertical stick" name="stick"></div>'+"\n";
+            grid_code += '<span id="'+r+'.5:'+c+'-o" class="owner"></span>'+"\n";
+         }
+         grid_code += '<div id="'+r+'.5:'+c+'" class="vertical stick" name="stick"><!-- Finishing --></div>'+"\n";
+         grid_code += ' <br/><!-- Complete vertical row -->'+"\n";
+      }
    }
-
-   grid_code += '</table>'+"\n";
 
 
    if (sReturn)
@@ -48,109 +54,160 @@ game.build_grid = function (size, sReturn){
 game.handle = new Object();
 
 game.handle.stickClicked = function (stick) {
-   var si = stick.parentNode.id;
-   var coords = si.split(":");
+   if (stick.className.indexOf("marked") != -1)
+      return false;
 
-   var sc = stick.className;
-   var pos;
-   if (sc.indexOf("top")    != -1) pos = 'top';
-   if (sc.indexOf("right")  != -1) pos = 'right';
-   if (sc.indexOf("bottom") != -1) pos = 'bottom';
-   if (sc.indexOf("left")   != -1) pos = 'left';
+   var coords = stick.id.split(":");
+
+   var vertical = (coords[0].substr(-2) == '.5');
+
+   if(vertical)
+      coords[0] = Math.floor( coords[0] );
+
 
    stick.className += " marked";
 
-   if (pos=='top') {
-      var nsi = ( (coords[0]-1)+":"+(coords[1]) );
-      var opositePos = 'bottom';
-   }
-   if (pos=='right') {
-      var nsi = ( (coords[0])+":"+(coords[1]-(-1)) );
-      var opositePos = 'left';
-   }
-   if (pos=='bottom') {
-      var nsi = ( (coords[0]-(-1))+":"+(coords[1]) );
-      var opositePos = 'top';
-   }
-   if (pos=='left') {
-      var nsi = ( (coords[0])+":"+(coords[1]-1) );
-      var opositePos = 'right';
-   }
 
-   var nBlock = document.getElementById(nsi);
-   if (nBlock) {
-      var sticks = nBlock.getElementsByTagName("*");
+   //Check if it completes a block and assign it to the owner
+   var extraMove = false;
+
+   var rep;
+   for (rep=0; rep<2; rep++) {
+
+      if (vertical) { // Vertical
+         if (rep == 0) {
+            var adj = [
+               ( coords[0]       +":"+ (coords[1]-1)),
+               ((coords[0]+".5") +":"+ (coords[1]-1)),
+               ((coords[0]-(-1)) +":"+ (coords[1]-1))
+            ];
+            var iOwner = coords[0]+".5:"+(coords[1]-1)+"-o";
+         }
+         if (rep == 1) {
+            var adj = [
+               ( coords[0]       +":"+  coords[1]),
+               ((coords[0]+".5") +":"+ (coords[1]-(-1))),
+               ((coords[0]-(-1)) +":"+  coords[1])
+            ];
+            var iOwner = coords[0]+".5:"+(coords[1])+"-o";
+         }
+      } else { //Horizontal
+         if (rep == 0) {
+            var adj = [
+               ((coords[0]-1)      +":"+  coords[1]),
+               ((coords[0]-1)+".5" +":"+  coords[1]),
+               ((coords[0]-1)+".5" +":"+ (coords[1]-(-1)))
+            ];
+            var iOwner = (coords[0]-1)+".5:"+(coords[1])+"-o";
+         }
+         if (rep == 1) {
+            var adj = [
+               ((coords[0]-(-1)) +":"+  coords[1]),
+               ( coords[0] +".5" +":"+  coords[1]),
+               ( coords[0] +".5" +":"+ (coords[1]-(-1)))
+            ];
+            var iOwner = (coords[0])+".5:"+(coords[1])+"-o";
+         }
+      }
+
+
+      var cMarked = 0;
 
       var i;
-      for (i=0; i<sticks.length; i++) {
-         if (sticks[i].className.indexOf("stick") != -1)
-            if (sticks[i].className.indexOf(opositePos) != -1)
-               sticks[i].className += " marked";
+      for (i=0; i<adj.length; i++) {
+         var nStick = document.getElementById( adj[i] );
+
+         if (!nStick)
+            continue;
+
+         if (nStick.className.indexOf("marked") != -1)
+            cMarked++;
+      }
+
+      if (cMarked >= 3) {
+         document.getElementById( iOwner ).innerHTML = game.current.player.mark;
+         game.current.player.blocks++;
+         extraMove = true;
       }
    }
 
-   //Check if it completes a block and assign it to the owner
-   game.checkBlock( stick.parentNode );
-   if (nBlock)
-      game.checkBlock( nBlock );
-
    //Register the marking
-   game.current.player = game.player( (game.current.player.turn-(-1)) % game.players.length );
+   if (!extraMove)
+      game.current.player = game.player( (game.current.player.turn-(-1)) % game.players.length );
    game.current.turn++;
+   game.current.markedSticks++;
+
+   //Update the notice
+   game.notice(0, game.current.player); //.notice(0  -> Y's turn notice
+
+   //Check if the game is over
+   if (game.current.markedSticks >= game.param.countSticks) {
+      game.notice(1, game.leadingPlayer()); //.notice(1  -> game over notice
+   }
 }
 
-game.checkBlock = function (block) {
-   var sticks = block.getElementsByTagName("*");
-   var ms = 0;
 
-   var i;
-   for (i=0; i<sticks.length; i++) {
-      if (sticks[i].className.indexOf("stick") != -1) {
-         if (sticks[i].className.indexOf("marked") != -1)
-            ms++;
-      } else
-      if (sticks[i].className.indexOf("owner") != -1)
-         var bOwner = sticks[i];
+game.notice = function (msg_code) {
+   if (msg_code == 0) {
+      //Y's turn notice
+      var pr = "<strong>"+arguments[1].name+"</strong>";
+      var msg = "It is "+pr+"'s turn";
+   }
+   if (msg_code == 1) {
+      //Y's turn notice
+      var msg = "<em>Game Over!</em>"+"\n<br />";
+      var pr = "<strong>"+arguments[1].name+"</strong>";
+      msg += pr+" has won!";
    }
 
-   if(ms==4 && bOwner) {
-      bOwner.innerHTML = game.current.player.mark;
-
-      //LOLOLOL compensation
-      block.style.position = 'relative';
-      block.style.top = '-0.5em';
-   }
+   document.getElementById("notice").innerHTML = msg;
+   return msg;
 }
 
 
 game.players  = new Array();
-game.marks = ['x', 'o', 'z', 'n', 'v', 'f', 'a', 's', 'k']; //and so on
-
-game.players.push(
-   {
-      mark: game.marks[game.players.length],
-      turn: game.players.length,
-      name: 'Player '+game.players.length
-   }
-);
-game.players.push(
-   {
-      mark: game.marks[game.players.length],
-      turn: game.players.length,
-      name: 'Player '+game.players.length
-   }
-);
-
 
 game.current = new Object();
 
 game.current.turn = 0; //default, should be reset by .initialize()
 game.current.player = game.players[0]; //default, should be reset by .initialize()
+game.current.markedSticks = 0;
 
 game.player = function (turn) {
    return game.players[turn];
 }
+game.leadingPlayer = function () {
+   var leading = null;
+   var m = new Array(); // multitude array for holding equally-leading players
 
+   var i;
+   for (i=0; i<game.players.length; i++) {
+      if (!leading) {
+         leading = game.players[i];
+         continue;
+      }
+
+      if (leading.blocks < game.players[i].blocks) {
+         leading = game.players[i];
+
+         m = new Array(); //clean multitude the array because those guys are not important anymore
+
+         continue;
+      }
+
+      if (leading.blocks == game.players[i].blocks) {
+         if (m.length == 0)
+            m.push( leading );
+
+         m.push( game.players[i] );
+      }
+   }
+
+   if (m.length > 0)
+      return m;
+
+   return leading;
+}
 
 game.initialize = function (){
    var sticks = document.getElementsByName('stick');
@@ -164,4 +221,6 @@ game.initialize = function (){
 
    game.current.player = game.player(0);
    game.current.turn = 0;
+
+   game.notice(0, game.current.player);
 }
--- index.php
+++ index.php
@@ -6,30 +6,99 @@
    <link rel="stylesheet" href="game.css" />
 
    <style type="text/css">
-   #grid{
+   .container{
       position: absolute;
       top: 50%;
       left: 0%;
       width: 100%;
       text-align: center;
-
-      font-size: 22px;
    }
    #grid{
+      font-size: 20px;
       margin-top: -6em;
    }
    </style>
 </head>
 <body>
 
-   <div id="grid"></div>
+<div class="container">
+
+   <div id="grid">
+      <div id="0:0" class="horizontal stick"></div>
+      <div id="0:1" class="horizontal stick"></div>
+      <div id="0:2" class="horizontal stick"></div>
+      <div id="0:3" class="horizontal stick"></div>
+      <div id="0:4" class="horizontal stick"></div>
+      <br /><!-- Line Break and rotate -->
+      <div id="0.5:0" class="vertical stick"></div>
+      <span id="1.5:0-o" class="owner"></span>
+      <div id="0.5:1" class="vertical stick"></div>
+      <span id="1.5:1-o" class="owner"></span>
+      <div id="0.5:2" class="vertical stick"></div>
+      <span id="1.5:2-o" class="owner"></span>
+      <div id="0.5:3" class="vertical stick"></div>
+      <span id="1.5:3-o" class="owner"></span>
+      <div id="0.5:4" class="vertical stick"></div>
+      <span id="1.5:4-o" class="owner"></span>
+      <div id="0.5:5" class="vertical stick"><!-- Finishing --></div>
+      <br/><!-- Line Break and reset -->
+
+      <div id="1:0" class="horizontal stick"></div>
+      <div id="1:1" class="horizontal stick"></div>
+      <div id="1:2" class="horizontal stick"></div>
+      <div id="1:3" class="horizontal stick"></div>
+      <div id="1:4" class="horizontal stick"></div>
+      <br /><!-- Line Break and rotate -->
+      <div id="1.5:0" class="vertical stick"></div>
+      <span id="1.5:0-o" class="owner">x</span>
+      <div id="1.5:1" class="vertical stick"></div>
+      <span id="1.5:1-o" class="owner">o</span>
+      <div id="1.5:2" class="vertical stick"></div>
+      <span id="1.5:2-o" class="owner">x</span>
+      <div id="1.5:3" class="vertical stick"></div>
+      <span id="1.5:3-o" class="owner">o</span>
+      <div id="1.5:4" class="vertical stick"></div>
+      <span id="1.5:4-o" class="owner">x</span>
+      <div id="1.5:5" class="vertical stick"><!-- Finishing --></div>
+      <br /><!-- Line Break and reset -->
+
+      <div id="1:0" class="horizontal stick"></div>
+      <div id="1:1" class="horizontal stick"></div>
+      <div id="1:2" class="horizontal stick"></div>
+      <div id="1:3" class="horizontal stick"></div>
+      <div id="1:4" class="horizontal stick"></div>
+      <!-- Finishing -->
+   </div>
 
    <div id="notice"></div>
+
+</div>
 
 </body>
 
 <script type="text/javascript">
-game.build_grid();
+//The below should be PHP generated
+game.marks = ['x', 'o', 'z', 'n', 'v', 'f', 'a', 's', 'k']; //and so on
+game.players.push(
+   {
+      mark: game.marks[game.players.length],
+      turn: game.players.length,
+      name: 'Player '+(game.players.length+1)+' ('+game.marks[game.players.length]+')',
+      blocks: 0
+   }
+);
+game.players.push(
+   {
+      mark: game.marks[game.players.length],
+      turn: game.players.length,
+      name: 'Player '+(game.players.length+1)+' ('+game.marks[game.players.length]+')',
+      blocks: 0
+   }
+);
+</script>
+
+<script type="text/javascript">
+game.build_grid(7);
 game.initialize();
 </script>
 
